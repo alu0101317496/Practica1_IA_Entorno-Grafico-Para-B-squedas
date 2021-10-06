6671a26271423fb2f7b08ac80c717648e1cc6d40
LucasHdezAbreu <alu0101317496@ull.edu.es>
2021-10-07 00:05:19 +0100

    almost finished backbuffer

--- source/main.cc
+++ source/main.cc
@@ -1,7 +1,7 @@
 /* =============================================================================
- // Creator: Lucas Hernández Abreu
-// Name: main.cc
-// Purpose: Creating the graphical interface talking with the API of Windows.
+ // @Creator: Lucas Hernández Abreu
+// @Name: main.cc
+// @Purpose: Creating the graphical interface talking with the API of Windows.
 //
        //============================================================================== */
 #include <windows.h>
@@ -14,36 +14,72 @@
 
 // TODO(Seriuusly): THIS IS A GLOBAL FOR NOW
 global_variable bool Running;
-
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
 
 internal void
-Win32ResizeDIBSection()
+Win32ResizeDIBSection(int Width, int Height)
 {
-    HBITMAP CreateDIBSection(
-                             HDC              hdc,
-                             BITMAPINFO       *pbmi,
-                             UINT             usage,
-                             VOID             **ppvBits,
-                             HANDLE           hSection,
-                             DWORD            offset
-                             );
+    /*
+@INFO: BitmapinfoHeader Structure
+typedef struct tagBITMAPINFOHEADER {
+  DWORD biSize;
+  LONG  biWidth;
+  LONG  biHeight;
+  WORD  biPlanes;
+  WORD  biBitCount;
+  DWORD biCompression;
+  DWORD biSizeImage;
+  LONG  biXPelsPerMeter;
+  LONG  biYPelsPerMeter;
+  DWORD biClrUsed;
+  DWORD biClrImportant;
+} BITMAPINFOHEADER, *PBITMAPINFOHEADER;
+*/
+    
+    if(BitmapHandle)
+    {
+        DeleteObject(BitmapHandle);
+    }
+    
+    if(!BitmapDeviceContext)
+    {
+        BitmapDeviceContext = CreateCompatibleDC(0);
+    }
+    
+    BITMAPINFO BitmapInfo = {};
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = Width;
+    BitmapInfo.bmiHeader.biHeight = Height;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+    BitmapInfo.bmiHeader.biSizeImage = 0;
+    BitmapInfo.bmiHeader.biXPelsPerMeter = 0;
+    BitmapInfo.bmiHeader.biYPelsPerMeter = 0;
+    BitmapInfo.bmiHeader.biClrUsed = 0;
+    BitmapInfo.bmiHeader.biClrImportant = 0;
+    
+    // TODO(Seriuusly): Free after if the creation succeded, if it fails user de old DIB
+    
+    HBITMAP BitmapHandle = CreateDIBSection(BitmapDeviceContext,
+                                            &BitmapInfo,
+                                            DIB_RGB_COLORS,
+                                            &BitmapMemory,
+                                            0, 0);
 }
 
 internal void
 Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
 {
-    StretchDIBits(
-                  DeviceContext,
+    StretchDIBits(DeviceContext,
                   X, Y, Width, Height,
                   X, Y, Width, Height,
-                  
-                  // TODO(Seriuusly): Vas POR AQuí 37:06 BACK BUFFER ALLOCATE NEGRATA
-                  
-                  const VOID       *lpBits,
-                  const BITMAPINFO *lpbmi,
-                  UINT             iUsage,
-                  DWORD            rop
-                  );
+                  BitmapMemory,
+                  &BitmapInfo,
+                  DIB_RGB_COLORS, SRCCOPY);
 }
 
 LRESULT CALLBACK 
@@ -62,7 +98,7 @@ MainWindowCallback(HWND Window,
             GetClientRect(Window, &ClientRect);
             int Width = ClientRect.right - ClientRect.left;
             int Height = ClientRect.top - ClientRect.bottom;;
-            ResizeDIBSection(Width, Height);
+            Win32ResizeDIBSection(Width, Height);
             OutputDebugStringA("WM_SIZE\n");
         }break;
         
@@ -95,9 +131,6 @@ MainWindowCallback(HWND Window,
             int Width = Paint.rcPaint.left - Paint.rcPaint.right;
             int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
             Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
-            
-            
-            
             
             EndPaint(Window, &Paint);
             
