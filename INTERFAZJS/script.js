c52e5150eaccf91c226e8a3fd876e06ab7c63e8a
Ian <alu0101333946@ull.edu.es>
2021-10-23 19:09:54 +0100

    Funcion lectura de fichero

--- INTERFAZJS/script.js
+++ INTERFAZJS/script.js
@@ -19,14 +19,16 @@ var spritesheetLoaded = false;
 // the world grid (2d array of tiles)
 var world = [[]];
 
-// --------------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// auxiliar value that switches between 0 and 1 if the user click the first
+// time (initial node) or the second (end node)
 var path = 0;
 
 // start and end of path
 var pathStart = [worldWidth, worldHeight];
 var pathEnd = [0, 0];
 var currentPath = [];
-
+var pathFound = false;
 
 // ensure that concole.log doesn't cause errors
 if (typeof console == "undefined") var console = {
@@ -87,8 +89,6 @@ function onload() {
 	} else {
 		spritesheet.src = './img/Tiles10x2.png'
 	}
-
-	console.log(size);	
 	spritesheet.onload = loaded;
 }
 
@@ -142,55 +142,6 @@ function createWorld() {
 	redrawWalls();
 }
 
-// Original redraw (not recomended to use, not optimal)
-function redraw() {
-	if (!spritesheetLoaded) {return};
-	var spriteNum = 0;
-
-	// clear the screen
-	ctx.fillStyle = '#000000';
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-	// draw world with walls
-	for (var x = 0; x < worldWidth; x++) {
-		for (var y = 0; y < worldHeight; y++) {
-			switch (world[x][y]) {
-				case 1:
-					spriteNum = 1;
-					break;
-				default:
-					spriteNum = 0;
-					break;
-			}
-			ctx.drawImage(spritesheet, spriteNum * tileWidth, 0,
-						  tileWidth, tileHeight, x * tileWidth,
-						  y * tileHeight, tileWidth, tileHeight);
-		}
-	}
-
-	// draw the path
-	for (rp = 0; rp < currentPath.length; rp++) {
-		switch (rp) {
-			case 0:
-				spriteNum = 2; // start
-				break;
-			case currentPath.length - 1:
-				spriteNum = 3; // end
-				break;
-			default:
-				spriteNum = 4; // path node
-				break;
-		}
-
-		ctx.drawImage(spritesheet,
-			spriteNum * tileWidth, 0,
-			tileWidth, tileHeight,
-			currentPath[rp][0] * tileWidth,
-			currentPath[rp][1] * tileHeight,
-			tileWidth, tileHeight);
-	}
-}
-
 // redraw only the walls
 function redrawWalls() {
 	if (!spritesheetLoaded) {return};
@@ -231,7 +182,7 @@ function redrawPath() {
 			currentPath[rp][0] * tileWidth,
 			currentPath[rp][1] * tileHeight,
 			tileWidth, tileHeight);
-	}	
+	}
 }
 
 // clear the old path
@@ -241,14 +192,21 @@ function clearPath() {
 
 	// clear path
 	for (rp = 0; rp < currentPath.length; rp++) {
-		spriteNum = 0;
-		ctx.drawImage(spritesheet,
+		switch(world[currentPath[rp][0]][currentPath[rp][1]]) {
+			case 0:
+				spriteNum = 0;
+				break;
+			case 1:
+				spriteNum = 1;
+				break;
+		}
+			ctx.drawImage(spritesheet,
 			spriteNum * tileWidth, 0,
 			tileWidth, tileHeight,
 			currentPath[rp][0] * tileWidth,
 			currentPath[rp][1] * tileHeight,
 			tileWidth, tileHeight);
-	}	
+	}
 }
 
 // clear the world
@@ -299,7 +257,13 @@ function rightClick(e) {
 	// recalculate path
 	clearPath();
 	currentPath = findPath(world, pathStart, pathEnd);
-	redrawPath();
+	if(pathFound) {
+		pathFound = false;
+		redrawPath();
+	}
+	else {
+		alert("Path not found");
+	}
 
 	// Prevent context menu
 	return false;
@@ -326,7 +290,7 @@ function canvasClick(e) {
 	x -= canvas.offsetLeft;
 	y -= canvas.offsetTop;
 
-	// return tile x,y that we clicked
+	// return tile x, y that we clicked
 	var cell = [
 		Math.floor(x / tileWidth),
 		Math.floor(y / tileHeight)
@@ -343,9 +307,6 @@ function canvasClick(e) {
 			currentPath = findPath(world, pathStart, pathEnd);
 		}
 
-		// now we know while tile we clicked
-		console.log('we clicked tile ' + cell[0] + ',' + cell[1]);
-
 		path = !path;
 
 		if (path) {
@@ -358,7 +319,18 @@ function canvasClick(e) {
 		// calculate path
 		currentPath = findPath(world, pathStart, pathEnd);
 	}
-	redrawPath();
+	else {
+		clearPath();
+		alert("This is an obtacle");
+	}
+	if (pathFound) {
+		pathFound = false;
+		redrawPath();
+	}
+	else {
+		clearPath();
+		alert("No path found")
+	}
 }
 
 // world is a 2d array of integers
@@ -366,12 +338,6 @@ function canvasClick(e) {
 function findPath(world, pathStart, pathEnd) {
 	// shortcuts for speed
 	var abs = Math.abs;
-
-	// the world data are integers:
-	// anything higher than this number is considered blocked
-	// this is handy is you use numbered sprites, more than one
-	// of which is walkable road, grass, mud, etc
-	var maxWalkableTileNum = 0;
 
 	// keep track of the world dimensions
 	var worldSize = worldWidth * worldHeight;
@@ -385,17 +351,17 @@ function findPath(world, pathStart, pathEnd) {
 	function ManhattanDistance(Point, Goal) {
 		return abs(Point.x - Goal.x) + abs(Point.y - Goal.y);
 	}
-	
+
 	function EuclideanDistance(Point, Goal) {
-		return Math.sqrt((abs(Goal.x - Point.x)) * (abs(Goal.x - Point.x)) + 
+		return Math.sqrt((abs(Goal.x - Point.x)) * (abs(Goal.x - Point.x)) +
 						 (abs(Goal.y - Point.y)) * (abs(Goal.y - Point.y)));
 	}
 
 	// Returns every available North, South, East or West
 	// cell that is empty. No diagonals,
 	function Neighbours(x, y) {
-		// declare de directions (N, S, E, W) and match them only if 
-		// the conditions are met respectively
+		// declare de directions (N, S, E, W) and match them only if
+		// the booleans respectively are true
 		var N = y - 1,
 			S = y + 1,
 			E = x + 1,
@@ -434,9 +400,9 @@ function findPath(world, pathStart, pathEnd) {
 
 	// returns boolean value (world cell is available and open)
 	function canWalkHere(x, y) {
-		return ((world[x] != null) &&
+		return ((world[x]    != null) &&
 				(world[x][y] != null) &&
-				(world[x][y] <= maxWalkableTileNum));
+				(world[x][y] == 0));
 	};
 
 	// Node function, returns a new object with Node properties
@@ -445,25 +411,24 @@ function findPath(world, pathStart, pathEnd) {
 		var newNode = {
 			// pointer to another Node object
 			Parent: Parent,
-			// array index of this Node in the world linear array
+			// array index of this Node in the world (linear array)
 			value: Point.x + (Point.y * worldWidth),
 			// the location coordinates of this Node
 			x: Point.x,
 			y: Point.y,
-			// the heuristic estimated cost
-			// of an entire path using this node
+			// the heuristic estimated cost of an entire path using this node
 			f: 0,
-			// the distanceFunction cost to get
-			// from the starting point to this node
-			g: 0
+			// the cost to get from the starting point to this node
+			g: 0,
+			// the distanceFunction cost to get from this node to the destination node
+			h: 0
 		};
-
 		return newNode;
 	}
 
 	// Path function, executes AStar algorithm operations
 	function calculatePath() {
-		// create Nodes from the Start and End x,y coordinates
+		// create Nodes from the Start and End x, y coordinates
 		var mypathStart = Node(null, {
 			x: pathStart[0],
 			y: pathStart[1]
@@ -481,7 +446,8 @@ function findPath(world, pathStart, pathEnd) {
 		var myPath;								// reference to a Node (that starts a path in question)
 		var length, max, min, i, j;				// temp integer variables used in the calculations
 
-		while (length = Open.length){			// iterate through the open list until none are left
+		// iterate through the open list until none are left
+		while (length = Open.length) {
 			max = worldSize;
 			min = -1;
 			for (i = 0; i < length; i++) {
@@ -490,9 +456,12 @@ function findPath(world, pathStart, pathEnd) {
 					min = i;
 				}
 			}
-			// grab the next node and remove it from Open array
+			// grab the next node and remove it from Open list array
 			myNode = Open.splice(min, 1)[0];
-			// is it the destination node?
+			// check if it is the destination node?
+			// The strict equality operator (===) checks whether its two operands are equal,
+			// returning a Boolean result. Unlike the equality operator, the strict equality
+			// operator always considers operands of different types to be different.
 			if (myNode.value === mypathEnd.value) {
 				myPath = Closed[Closed.push(myNode) - 1];
 				do {
@@ -503,6 +472,7 @@ function findPath(world, pathStart, pathEnd) {
 				AStar = Closed = Open = [];
 				// we want to return start to finish
 				result.reverse();
+				pathFound = true;
 			}
 			else { // not the destination
 				// find which nearby nodes are walkable
@@ -530,3 +500,77 @@ function findPath(world, pathStart, pathEnd) {
 	return calculatePath();
 }
 /*------------------------------------------------------------------------------------------------------------------------------------ */
+
+var obstaclesX = []; //File vector for X positions
+var obstaclesY = []; //File vector for Y positions
+var selector = 0; //Aux to choose X or Y file positions
+
+function pushObstacleX(string) {
+	obstaclesX.push(string)
+}
+
+function pushObstacleY(string) {
+	obstaclesY.push(string)
+}
+
+
+function process(event) {
+  var files = event.target.files
+  var reader = new FileReader()
+
+  reader.onload = function() {
+    var contents = this.result
+
+    for (let j = 0; j < contents.length; j++) {
+      //Initialize variable
+      var string = '';
+
+      //Initialize string
+      if (contents.substr(j, 1) >= '0' && contents.substr(j, 1) <='9' ) {
+        do {
+          string += contents.substr(j, 1); //Grow string if posible
+          j++;
+        }
+        while (contents.substr(j, 1) >= '0' && contents.substr(j, 1) <='9') //Check if the string is followed by numbers
+
+				//Choose to what vector push the string: X(pair) Y(odd)
+        if (selector % 2 == 0) {
+          pushObstacleX(string)
+        } else {
+          pushObstacleY(string)
+        }
+
+				//Upgrade selector
+        selector++;
+      }
+    }
+
+		//If there are uneven numbers (One X without Y), unuse the last X
+    if (obstaclesX.length > obstaclesY.length) {
+      obstaclesX.pop();
+    }
+
+    console.log(obstaclesX, obstaclesX.length);
+    console.log(obstaclesY, obstaclesY.length);
+
+		//Signal what positions to change as wall and call redraw function
+		for (itr = 0; itr < obstaclesX.length; itr++) {
+			x = obstaclesX[itr];
+			y = obstaclesY[itr];
+			console.log("Posicion x:", x, "y:",y);
+
+			world[x] = [];
+		  world[x][y] = 1;
+			redrawWalls();
+
+		}
+		obstaclesX = [];
+		obstaclesY = [];
+  }
+
+  reader.readAsText(files[0])
+}
+
+var input = document.querySelector('.file')
+document.addEventListener('change', process, false)
+------------------------------------------------------------------------------------------- */
